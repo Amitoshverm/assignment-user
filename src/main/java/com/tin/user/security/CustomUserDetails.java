c83a3dbf961f3ff4f5c439a1f8e2ad2f32247057
Amitoshverm <amitosh.verma9@gmail.com>
2026-01-30 23:12:51 +0530

    added our user which implements userdetailservice and removed hardcoded user

--- src/main/java/com/tin/user/security/CustomUserDetails.java
+++ src/main/java/com/tin/user/security/CustomUserDetails.java
@@ -1,4 +1,53 @@
 package com.tin.user.security;
 
-public class CustomUserDetails {
+import com.tin.user.models.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 }
